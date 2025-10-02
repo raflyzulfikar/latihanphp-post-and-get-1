02d592d74968f81c729684877f501dc33386a3d0
Rafly Zulfikar Al Kautsar <111392348+raflyzulfikar@users.noreply.github.com>
2025-10-02 17:32:31 +0700

    Add latihan 1_php

---
+++ aksi_latihan_1_post.php
@@ -0,0 +1,8 @@
+<?php
+    echo "Ini halaman yang berfungsi untuk menerima data yang dikirimkan menggunakan Method POST";
+    echo "<br>";
+    echo $_POST['nama_lengkap'];
+    echo "<br>";
+    echo $_POST['nim'];
+?>
+
---
+++ form_latihan_1_post.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Pemrograman PHP menggunakan Method POST dan GET</title>
+    <head>
+<body>
+    <form action="aksi_latihan_1_post.php" Method="POST">
+        <p>
+            <label>Nama Lengkap:</label>
+            <input type="text" name="nama_lengkap" placeholder="Masukkan Nama Lengkap">
+        </p>
+        <p>
+            <label>NIM:</label>
+            <input type="text" name="nim" placeholder="Masukkan NIM">
+        </p>
+        <p>
+            <input type="submit" name="submit" value="Kirim Data">
+        </p>
+    </form>
+</body>
+</html>
